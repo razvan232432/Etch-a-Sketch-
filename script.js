6361ebc67244b44f3e8802b3297c33637e08d099
razvan232432 <marius3837@proton.me>
2022-12-21 06:17:57 -0500

    Script js

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
-// Sets important constants and variables
+let clear = document.querySelector("button");
 
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+//function to create a 16x16 grid
+function createGrid(size){
+    function resetSize(){
+        clear.addEventListener('click', ()=>{
+            let number = prompt("What size would you like the grid to be? (1-100)");
+            container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+            container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+        })
+        
+        
+    }
+    resetSize();
 
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+    let container = document.querySelector(".container");
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    
+    for (let i = 0; i < size*size; i++) {
+        
+        let square = document.createElement("div");
+        square.style.backgroundColor = "black";
+        container.appendChild(square);
+
+        //change background color of a square on hover
+        square.addEventListener('mouseover', e=>{
+            square.style.backgroundColor = "white";
+        })
+
+
+        //function to reset the grid
+        function clearGrid(){
+            clear.addEventListener('click', e=>{
+                square.style.backgroundColor = "black"
+           })
+        }
+        
+
+        clearGrid();    
+    }
 }
 
-// Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-
-    // Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-// Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
+createGrid(16);
